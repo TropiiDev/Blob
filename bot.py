6c4647cc3e301baeb46738b2d4165da9cecacd8a
tropii <97747461+TropiiDev@users.noreply.github.com>
2023-11-24 14:53:08 -0500

    Added ALERT_MESSAGE

--- bot.py
+++ bot.py
@@ -66,11 +66,11 @@ class MyBot(commands.Bot):
     async def on_ready(self):
         await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"over {len(self.guilds)} servers!"))
 
-        guild = bot.get_guild(1177434657604775936)
+        guild = self.guilds
 
         # find the channel named "commands"
         owner = guild.owner
-        await owner.send("Hello Again!\n\nThis is an official message from the developer of Blob. We are continuing development of Blob and it will resume normal operations shortly.\n\nA lot has changed since the last time Blob was online. We are here to address your issues with our new discord server. Please join [now](https://discord.gg/A2EQDvA6sN).\n\nI (Tropii) hate to send messages to server owners like you with stuff like this, I do apologize. For further information please join the support server below!\n\nTake care - Tropii.\n\nHave any conerns? Feel free to add me `fstropii`")
+        await owner.send("Hello Again!\n\nThis is an official message from the developer of Blob. We are continuing development of Blob and it will resume normal operations shortly.\n\nA lot has changed since the last time Blob was online. We are here to address your issues with our new discord server. Please join [now](https://discord.gg/A2EQDvA6sN).\n\nI (Tropii) hate to send messages to server owners like you with stuff like this, I do apologize. For further information please join the support server below!\n\nTake care - Tropii.\n\nHave any conerns? Feel free to add me `fstropii`\n\nIf you see this message more than once please let me know ASAP!")
 
 # making var for commands in this file
 bot = MyBot()
